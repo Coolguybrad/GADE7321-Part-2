6cee8c4db1902475bf4551b601376012b11b9a57
Kerwin Naidoo <kerwinjn18@gmail.com>
2024-05-01 18:23:01 +0200

    Maintenance
    -Deleted unnecessary scripts
    - Updated piece textboxes to fit bushwalker text

--- Assets/Scripts/Managers/TurnHandler.cs
+++ Assets/Scripts/Managers/TurnHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardLayout : MonoBehaviour
+public class TurnHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
